dc575f331ea2ec734143b8fdcff63dc66e951c05
BolluSravani <sravani.bollu21@gmail.com>
2024-08-24 21:43:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
 provider "aws" {
   region  = "us-east-1"
 }
+# VPC
+resource "aws_vpc" "main7" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Subnets
+resource "aws_subnet" "public7" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main7.id
+  cidr_block              = cidrsubnet(aws_vpc.main7.cidr_block, 8, count.index)
+  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
+  map_public_ip_on_launch = true
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw7" {
+  vpc_id = aws_vpc.main7.id
+}
