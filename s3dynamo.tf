e554ec11c4fc432c928a4cda75c7883c86942e81
BolluSravani <sravani.bollu21@gmail.com>
2024-08-26 12:27:00 +0530

    Update s3dynamo.tf

--- s3dynamo.tf
+++ s3dynamo.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "Bollu-AWSterraformtest-state"  # Ensure this bucket name is globally unique
+  bucket = "Bollu297-terraformtest-state"  # Ensure this bucket name is globally unique
   acl    = "private"
   tags = {
     Name = "Terraform State Bucket"
