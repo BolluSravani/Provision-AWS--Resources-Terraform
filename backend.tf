6fe9a3b2636560f0410a31baa380dd543ca06dd7
BolluSravani <sravani.bollu21@gmail.com>
2024-08-26 10:26:27 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "sravani-jenkins-terraform-state"
-    key            = "terraform/state.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
+
