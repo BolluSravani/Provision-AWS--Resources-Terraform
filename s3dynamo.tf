8c18bf5e97e64e4ed2d5bfab4ce486770b24acca
BolluSravani <sravani.bollu21@gmail.com>
2024-08-26 12:05:15 +0530

    Update s3dynamo.tf

--- s3dynamo.tf
+++ s3dynamo.tf
@@ -1,13 +1,13 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "sravani-jenkins-terraform-state"  # Ensure this bucket name is globally unique
+  bucket = "sravaniterraform-state"  # Ensure this bucket name is globally unique
   acl    = "private"
   tags = {
     Name = "Terraform State Bucket"
   }
 }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name           = "terraform-locks"
+resource "aws_dynamodb_table" "terraform_statelocks" {
+  name           = "terraform-statelocks"
   billing_mode    = "PAY_PER_REQUEST"  # Use on-demand pricing
   hash_key        = "LockID"
   attribute {
