3f9cff231b24ef80d3649acdcf1c86c402ddd477
BolluSravani <sravani.bollu21@gmail.com>
2024-08-26 12:39:56 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "BolluS-terraform-Jenkinstest-state"
+    bucket         = "bollusterraform-jenkinstest-state"
     key            = "terraform/state.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-statelocks"
